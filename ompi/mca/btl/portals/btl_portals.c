bringhurst/ompi

commit 27bfb0f0bf8db1808f558faa7085052353a2cc5b

Finally the convertor export a convenience function to allow a consistent computation of the current location on the pack/unpack process. This can be used both for retrieving the pointer to the first byte (in the special case of the cached RDMA protocol) and for getting the current position (for the pipelined protocol).
I modified all BTLs, but most of them are still untested.



git-svn-id: http://svn.open-mpi.org/svn/ompi/trunk@14180 63e3feb5-37d5-0310-a306-e8a459e722fe

ompi/mca/btl/portals/btl_portals.c
@@ -431,7 +431,6 @@ mca_btl_portals_prepare_dst(struct mca_btl_base_module_t* btl_base,
     ptl_md_t md;
     ptl_handle_me_t me_h;
     int ret;
-    ptrdiff_t lb;
 
     assert(&mca_btl_portals_module == (mca_btl_portals_module_t*) btl_base);
 
@@ -448,9 +447,8 @@ mca_btl_portals_prepare_dst(struct mca_btl_base_module_t* btl_base,
         return NULL;
     }
 
-    ompi_ddt_type_lb(convertor->pDesc, &lb);
     frag->segments[0].seg_len = *size;
-    frag->segments[0].seg_addr.pval = convertor->pBaseBuf + lb + convertor->bConverted;
+    ompi_convertor_get_current_pointer( convertor, (void**)&(frag->segment.seg_addr.pval) );
     frag->segments[0].seg_key.key64 = 
         OPAL_THREAD_ADD64(&(mca_btl_portals_module.portals_rdma_key), 1);
     frag->base.des_src = NULL;

ompi/datatype/convertor.c
@@ -235,12 +235,12 @@ int32_t ompi_convertor_pack( ompi_convertor_t* pConv,
          * use the bConverted to manage the conversion.
          */
         uint32_t i;
-        char* base_pointer;
+        unsigned char* base_pointer;
         size_t pending_length = pConv->local_size - pConv->bConverted;
 
         *max_data = pending_length;
-        base_pointer = pConv->pBaseBuf + pConv->bConverted + 
-            pConv->use_desc->desc[pConv->use_desc->used].end_loop.first_elem_disp;
+        ompi_convertor_get_current_pointer( pConv, (void**)&base_pointer );
+
         for( i = 0; i < *out_size; i++ ) {
             if( iov[i].iov_len >= pending_length ) {
                 goto complete_contiguous_data_pack;
@@ -282,12 +282,12 @@ int32_t ompi_convertor_unpack( ompi_convertor_t* pConv,
          * use the bConverted to manage the conversion.
          */
         uint32_t i;
-        char* base_pointer;
+        unsigned char* base_pointer;
         size_t pending_length = pConv->local_size - pConv->bConverted;
 
         *max_data = pending_length;
-        base_pointer = pConv->pBaseBuf + pConv->bConverted + 
-            pConv->use_desc->desc[pConv->use_desc->used].end_loop.first_elem_disp;
+        ompi_convertor_get_current_pointer( pConv, (void**)&base_pointer );
+
         for( i = 0; i < *out_size; i++ ) {
             if( iov[i].iov_len >= pending_length ) {
                 goto complete_contiguous_data_unpack;

ompi/datatype/convertor.h
@@ -84,7 +84,7 @@ struct ompi_convertor_t {
     const struct ompi_datatype_t* pDesc;        /**< the datatype description associated with the convertor */
     const struct dt_type_desc*    use_desc;     /**< the version used by the convertor (normal or optimized) */
     uint32_t                      count;        /**< the total number of full datatype elements */
-    char*                         pBaseBuf;     /**< initial buffer as supplied by the user */
+    unsigned char*                pBaseBuf;     /**< initial buffer as supplied by the user */
     dt_stack_t*                   pStack;       /**< the local stack for the actual conversion */
     uint32_t                      stack_size;   /**< size of the allocated stack */
     convertor_advance_fct_t       fAdvance;     /**< pointer to the pack/unpack functions */
@@ -188,6 +188,19 @@ ompi_convertor_get_unpacked_size( const ompi_convertor_t* pConv,
 }
 
 /**
+ * Return the current absolute position of the next pack/unpack. This function is
+ * mostly useful for contiguous datatypes, when we need to get the pointer to the
+ * contiguous piece of memory.
+ */
+static inline void
+ompi_convertor_get_current_pointer( const ompi_convertor_t* pConv,
+                                    void** position )
+{
+    unsigned char* base = pConv->pBaseBuf + pConv->bConverted + pConv->pDesc->true_lb;
+    *position = (void*)base;
+}
+
+/**
  * Basic convertor initialization. This is performed for all types of convertors,
  * but is the only thing required for convertors working on contiguous or
  * predefined datatyps.
@@ -196,7 +209,7 @@ ompi_convertor_get_unpacked_size( const ompi_convertor_t* pConv,
     {                                                                   \
         /* Compute the local in advance */                              \
         convertor->local_size = count * datatype->size;                 \
-        convertor->pBaseBuf   = (char*)pUserBuf;                        \
+        convertor->pBaseBuf   = (unsigned char*)pUserBuf;               \
         convertor->count      = count;                                  \
                                                                         \
         /* Grab the datatype part of the flags */                       \

ompi/datatype/datatype_pack.c
@@ -56,7 +56,7 @@ ompi_pack_homogeneous_contig_function( ompi_convertor_t* pConv,
                                        size_t* max_data )
 {
     dt_stack_t* pStack = pConv->pStack;
-    char *source_base = NULL;
+    unsigned char *source_base = NULL;
     uint32_t iov_count;
     size_t length = pConv->local_size - pConv->bConverted, initial_amount = pConv->bConverted;
     ptrdiff_t initial_displ = pConv->use_desc->desc[pConv->use_desc->used].end_loop.first_elem_disp;
@@ -103,7 +103,7 @@ ompi_pack_homogeneous_contig_with_gaps_function( ompi_convertor_t* pConv,
 {
     const ompi_datatype_t* pData = pConv->pDesc;
     dt_stack_t* pStack = pConv->pStack;
-    char *user_memory, *packed_buffer;
+    unsigned char *user_memory, *packed_buffer;
     uint32_t i, index, iov_count;
     size_t max_allowed, total_bytes_converted = 0;
     ptrdiff_t extent;
@@ -254,7 +254,7 @@ ompi_generic_simple_pack_function( ompi_convertor_t* pConvertor,
     dt_elem_desc_t* description;
     dt_elem_desc_t* pElem;
     const ompi_datatype_t *pData = pConvertor->pDesc;
-    char *source_base, *destination;
+    unsigned char *source_base, *destination;
     size_t iov_len_local;
     uint32_t iov_count;
 

ompi/datatype/datatype_pack.h
@@ -16,14 +16,14 @@
 static inline void pack_predefined_data( ompi_convertor_t* CONVERTOR,
                                          dt_elem_desc_t* ELEM,
                                          uint32_t* COUNT,
-                                         char** SOURCE,
-                                         char** DESTINATION,
+                                         unsigned char** SOURCE,
+                                         unsigned char** DESTINATION,
                                          size_t* SPACE )
 {
     uint32_t _copy_count = *(COUNT);
     size_t _copy_blength;
     ddt_elem_desc_t* _elem = &((ELEM)->elem);
-    char* _source = (*SOURCE) + _elem->disp;
+    unsigned char* _source = (*SOURCE) + _elem->disp;
 
     _copy_blength = ompi_ddt_basicDatatypes[_elem->common.type]->size;
     if( (_copy_count * _copy_blength) > *(SPACE) ) {
@@ -62,13 +62,13 @@ static inline void pack_predefined_data( ompi_convertor_t* CONVERTOR,
 static inline void pack_contiguous_loop( ompi_convertor_t* CONVERTOR,
                                          dt_elem_desc_t* ELEM,
                                          uint32_t* COUNT,
-                                         char** SOURCE,
-                                         char** DESTINATION,
+                                         unsigned char** SOURCE,
+                                         unsigned char** DESTINATION,
                                          size_t* SPACE )
 {
     ddt_loop_desc_t *_loop = (ddt_loop_desc_t*)(ELEM);
     ddt_endloop_desc_t* _end_loop = (ddt_endloop_desc_t*)((ELEM) + _loop->items);
-    char* _source = (*SOURCE) + _end_loop->first_elem_disp;
+    unsigned char* _source = (*SOURCE) + _end_loop->first_elem_disp;
     uint32_t _copy_loops = *(COUNT);
     uint32_t _i;
 

ompi/datatype/datatype_unpack.c
@@ -186,7 +186,7 @@ ompi_unpack_homogeneous_contig_function( ompi_convertor_t* pConv,
                                          size_t* max_data )
 {
     const ompi_datatype_t *pData = pConv->pDesc;
-    char *user_memory, *packed_buffer;
+    unsigned char *user_memory, *packed_buffer;
     uint32_t iov_count, i;
     size_t bConverted, remaining, length, initial_bytes_converted = pConv->bConverted;
     dt_stack_t* stack = pConv->pStack;
@@ -196,7 +196,7 @@ ompi_unpack_homogeneous_contig_function( ompi_convertor_t* pConv,
     DO_DEBUG( opal_output( 0, "unpack_homogeneous_contig( pBaseBuf %p, iov_count %d )\n",
                            pConv->pBaseBuf, *out_size ); );
     for( iov_count = 0; iov_count < (*out_size); iov_count++ ) {
-        packed_buffer = (char*)iov[iov_count].iov_base;
+        packed_buffer = (unsigned char*)iov[iov_count].iov_base;
         remaining = pConv->local_size - pConv->bConverted;
         if( remaining > (uint32_t)iov[iov_count].iov_len )
             remaining = iov[iov_count].iov_len;
@@ -284,13 +284,13 @@ ompi_unpack_homogeneous_contig_function( ompi_convertor_t* pConv,
  */
 static inline uint32_t
 ompi_unpack_partial_datatype( ompi_convertor_t* pConvertor, dt_elem_desc_t* pElem,
-                              char* partial_data,
+                              unsigned char* partial_data,
                               ptrdiff_t start_position, ptrdiff_t end_position,
-                              char** user_buffer )
+                              unsigned char** user_buffer )
 {
     char unused_byte = 0x7F, saved_data[16];
-    char temporary[16], *temporary_buffer = temporary;
-    char* real_data = *user_buffer + pElem->elem.disp;
+    unsigned char temporary[16], *temporary_buffer = temporary;
+    unsigned char* real_data = *user_buffer + pElem->elem.disp;
     uint32_t i, length, count_desc = 1;
     size_t data_length = ompi_ddt_basicDatatypes[pElem->elem.common.type]->size;
 
@@ -356,7 +356,7 @@ ompi_generic_simple_unpack_function( ompi_convertor_t* pConvertor,
     dt_elem_desc_t* description;
     dt_elem_desc_t* pElem;
     const ompi_datatype_t *pData = pConvertor->pDesc;
-    char *user_memory_base, *packed_buffer;
+    unsigned char *user_memory_base, *packed_buffer;
     size_t iov_len_local;
     uint32_t iov_count;
 
@@ -421,7 +421,7 @@ ompi_generic_simple_unpack_function( ompi_convertor_t* pConvertor,
                 type = pElem->elem.common.type;
                 assert( type < DT_MAX_PREDEFINED );
                 if( 0 != iov_len_local ) {
-                    char* temp = user_memory_base;
+                    unsigned char* temp = user_memory_base;
                     /* We have some partial data here. Let's copy it into the convertor
                      * and keep it hot until the next round.
                      */
@@ -440,7 +440,7 @@ ompi_generic_simple_unpack_function( ompi_convertor_t* pConvertor,
             if( DT_END_LOOP == pElem->elem.common.type ) { /* end of the current loop */
                 DO_DEBUG( opal_output( 0, "unpack end_loop count %d stack_pos %d pos_desc %d disp %ld space %lu\n",
                                        (int)pStack->count, pConvertor->stack_pos, pos_desc,
-                                      (long)pStack->disp, (unsigned long)iov_len_local ); );
+                                       (long)pStack->disp, (unsigned long)iov_len_local ); );
                 if( --(pStack->count) == 0 ) { /* end of loop */
                     if( pConvertor->stack_pos == 0 ) {
                         /* Force the conversion to stop by lowering the number of iovecs. */

ompi/datatype/datatype_unpack.h
@@ -16,14 +16,14 @@
 static inline void unpack_predefined_data( ompi_convertor_t* CONVERTOR, /* the convertor */
                                            dt_elem_desc_t* ELEM,         /* the element description */
                                            uint32_t* COUNT,              /* the number of elements */
-                                           char** SOURCE,                /* the source pointer */
-                                           char** DESTINATION,           /* the destination pointer */
+                                           unsigned char** SOURCE,       /* the source pointer */
+                                           unsigned char** DESTINATION,  /* the destination pointer */
                                            size_t* SPACE )               /* the space in the destination buffer */
 {
     uint32_t _copy_count = *(COUNT);
-	size_t _copy_blength;
+    size_t _copy_blength;
     ddt_elem_desc_t* _elem = &((ELEM)->elem);
-    char* _destination = (*DESTINATION) + _elem->disp;
+    unsigned char* _destination = (*DESTINATION) + _elem->disp;
 
     _copy_blength = ompi_ddt_basicDatatypes[_elem->common.type]->size;
     if( (_copy_count * _copy_blength) > *(SPACE) ) {
@@ -62,13 +62,13 @@ static inline void unpack_predefined_data( ompi_convertor_t* CONVERTOR, /* the c
 static inline void unpack_contiguous_loop( ompi_convertor_t* CONVERTOR,
                                            dt_elem_desc_t* ELEM,
                                            uint32_t* COUNT,
-                                           char** SOURCE,
-                                           char** DESTINATION,
+                                           unsigned char** SOURCE,
+                                           unsigned char** DESTINATION,
                                            size_t* SPACE )
 {
     ddt_loop_desc_t *_loop = (ddt_loop_desc_t*)(ELEM);
     ddt_endloop_desc_t* _end_loop = (ddt_endloop_desc_t*)((ELEM) + _loop->items);
-    char* _destination = (*DESTINATION) + _end_loop->first_elem_disp;
+    unsigned char* _destination = (*DESTINATION) + _end_loop->first_elem_disp;
     uint32_t _copy_loops = *(COUNT);
     uint32_t _i;
 

ompi/datatype/dt_optimize.c
@@ -279,8 +279,8 @@ int32_t ompi_ddt_commit( ompi_datatype_t** data )
         dt_elem_desc_t* pElem = pData->desc.desc;
 
         index = GET_FIRST_NON_LOOP( pElem );
-        assert( pData->desc.desc[index].elem.common.flags & DT_FLAG_DATA );
-        first_elem_disp = pData->desc.desc[index].elem.disp;
+        assert( pElem[index].elem.common.flags & DT_FLAG_DATA );
+        first_elem_disp = pElem[index].elem.disp;
     }
 
     /* let's add a fake element at the end just to avoid useless comparaisons

commit 0750f0d16eca9b27cd312132eab3bb875dbe9896

Correct an error introduced by commit r14180.
git-svn-id: http://svn.open-mpi.org/svn/ompi/trunk@14191 63e3feb5-37d5-0310-a306-e8a459e722fe

ompi/mca/btl/portals/btl_portals.c
@@ -448,7 +448,7 @@ mca_btl_portals_prepare_dst(struct mca_btl_base_module_t* btl_base,
     }
 
     frag->segments[0].seg_len = *size;
-    ompi_convertor_get_current_pointer( convertor, (void**)&(frag->segment.seg_addr.pval) );
+    ompi_convertor_get_current_pointer( convertor, (void**)&(frag->segments[0].seg_addr.pval) );
     frag->segments[0].seg_key.key64 = 
         OPAL_THREAD_ADD64(&(mca_btl_portals_module.portals_rdma_key), 1);
     frag->base.des_src = NULL;